8a79a8a9c69ab50c6a340e335dd7f3d70ab2fc82
Rumen Guin <rumen2k@gmail.com>
2021-12-15 09:54:04 +0530

    Initial Commit

---
+++ SwiftUIBootcamp/SwiftUIBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootcampApp.swift
+//  SwiftUIBootcamp
+//
+//  Created by RUMEN GUIN on 15/12/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
