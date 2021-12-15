f456177f8dc19703c337fcaa69bc05c918073f2b
Rumen Guin <rumen2k@gmail.com>
2021-12-15 22:52:24 +0530

    stack 2

--- SwiftUIBootcamp/StacksBootcamp.swift
+++ SwiftUIBootcamp/StacksBootcamp.swift
@@ -34,33 +34,33 @@ struct StacksBootcamp: View {
             Rectangle()
                 .fill(Color.yellow)
                 .frame(width: 350, height: 500)
-            
+
             VStack(alignment: .leading, spacing: 30){
-                
+
                 Rectangle()
                     .fill(Color.red)
                 .frame(width: 150, height: 150)
-            
-                
+
+
                 Rectangle()
                     .fill(Color.green)
                     .frame(width: 100, height: 100)
-                
+
                 HStack(alignment: .bottom, spacing: 16) {
                     Rectangle()
                         .fill(Color.purple)
                         .frame(width: 50, height: 50)
-                    
+
                     Rectangle()
                         .fill(Color.pink)
                         .frame(width: 75, height: 75)
-                    
+
                     Rectangle()
                         .fill(Color.blue)
                         .frame(width: 40, height: 40)
                 }
                 .background(Color.white)
-                
+
                 HStack(alignment: .top){
                     Text("Items in your cart:")
                         .font(.title3)
@@ -69,11 +69,33 @@ struct StacksBootcamp: View {
                         .font(.largeTitle)
                         .foregroundColor(.yellow)
                 }
-        
+
             }
             .background(Color.black)
-                
+
         }
+        
+        
+        //background <-> ZStack
+//        VStack(spacing: 50){
+//
+//            ZStack{
+//                Circle()
+//                    .frame(width: 100, height: 100)
+//                Text("1")
+//                    .font(.largeTitle)
+//                    .foregroundColor(.white)
+//            }
+//
+//
+//            Text("1")
+//                .font(.largeTitle)
+//                .foregroundColor(.white)
+//                .background(
+//                    Circle()
+//                        .frame(width: 100, height: 100)
+//                )
+//        }
     }
 }
 
