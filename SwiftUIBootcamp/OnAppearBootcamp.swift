11171cfb9dad7d2ee331947e8363cbfacaf199a0
Rumen Guin <rumen2k@gmail.com>
2021-12-20 17:58:51 +0530

    on appear commit

---
+++ SwiftUIBootcamp/OnAppearBootcamp.swift
@@ -0,0 +1,49 @@
+//
+//  OnAppearBootcamp.swift
+//  SwiftUIBootcamp
+//
+//  Created by RUMEN GUIN on 20/12/21.
+//
+
+import SwiftUI
+
+struct OnAppearBootcamp: View {
+    @State var myText: String = "Start text"
+    @State var count: Int = 0
+    var body: some View {
+        NavigationView{
+            ScrollView{
+                Text(myText)
+                LazyVStack {
+                    ForEach(0..<50) { _ in
+                        RoundedRectangle(cornerRadius: 25)
+                            .frame(height: 200)
+                            .padding()
+                            .onAppear {
+                                count += 1
+                            }
+                    }
+                }
+            }
+            .onAppear(perform: {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                    myText = "This is the new text"
+                }
+                
+            })
+            
+            .onDisappear(perform: {
+                myText = "Ending text"
+            })
+            .navigationTitle("On Appear \(count)")
+        }
+    }
+    
+    
+}
+
+struct OnAppearBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        OnAppearBootcamp()
+    }
+}
